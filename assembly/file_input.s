4f1b2b2961b4a40b543830dbd8dc02c32a68146d
Filipe Marinho <40862255+filipemarinho@users.noreply.github.com>
2020-06-15 16:29:18 -0300

    Update file_input.s

--- assembly/file_input.s
+++ assembly/file_input.s
@@ -26,46 +26,46 @@ sBuffer1:    .skip    BUFFERSIZE
 
 .text
 .global _start
-_start:                      // entry of program
+_start:
 
     //initial message
     ldr x0,qAdrszMessDeb
-    bl affichageMess
-    mov x0,0           // Linux input console stdin = 0
-    ldr x1,qAdrsBuffer     // buffer address 
-    mov x2,BUFFERSIZE      // buffer size 
-    mov x8,63            // request to read datas read = 63
-    svc 0                  // call system
+    bl affichageMess		//format and print, x0 contains the address
+    mov x0,0			// Linux input console stdin = 0
+    ldr x1,qAdrsBuffer     	// buffer address 
+    mov x2,BUFFERSIZE      	// buffer size 
+    mov x8,63            	// request to read datas sys_read = 63
+    svc 0                  	// call system
     
-    ldr x1,qAdrsBuffer     // buffer address 
-    mov x2,#0              // end of string
-    sub x0,x0,#1 	  // x0 contains the len, x0-1 will insert the byte at the last digit
-    strb w2,[x1,x0]        // store byte at the end of input string (x0 contains number of characters)
+    ldr x1,qAdrsBuffer     	// buffer address 
+    mov x2,#0              	// end of string
+    sub x0,x0,#1 	  	// x0 contains the len, x0-1 will insert the byte at the last digit
+    strb w2,[x1,x0]        	// store byte at the last digit input string (x0 = len-1)
  
-    ldr x0,qAdrsBuffer     // buffer address 
-    bl affichageMess
-    ldr x0,qAdrszCarriageReturn   
-    bl affichageMess
+    ldr x0,qAdrsBuffer     	// buffer address 
+    bl affichageMess		//format and print, x0 contains the address
+    ldr x0,qAdrszCarriageReturn //print \n   
+    bl affichageMess		//format and print, x0 contains the address
     
     
     //second message 
-    ldr x0,qAdrszMessNum
-    bl affichageMess
-    mov x0,0           // Linux input console stdin = 0
-    ldr x1,qAdrsBuffer1     // buffer address 
-    mov x2,BUFFERSIZE      // buffer size 
-    mov x8,63            // request to read datas read = 63
-    svc 0                  // call system
-    ldr x1,qAdrsBuffer1     // buffer address 
-    mov x2,#0              // end of string
-    strb w2,[x1,x0]        // store byte at the end of input string (x0
+    ldr x0,qAdrszMessNum	// load message address
+    bl affichageMess		// format and print, x0 contains the address
+    mov x0,0           		// Linux input console stdin = 0
+    ldr x1,qAdrsBuffer1     	// buffer address 
+    mov x2,BUFFERSIZE      	// buffer size 
+    mov x8,63            	// request to read datas sys_read = 63
+    svc 0                  	// call system
+    ldr x1,qAdrsBuffer1     	// buffer address 
+    mov x2,#0              	// end of string
+    strb w2,[x1,x0]        	// store byte at the end of input string
     
-    mov x20,x0
+    mov x20,x0			// save len to write
     
-    ldr x0,qAdrsBuffer1     // buffer address 
-    bl affichageMess
-    ldr x0,qAdrszCarriageReturn   
-    bl affichageMess
+    ldr x0,qAdrsBuffer1     	// buffer address 
+    bl affichageMess		// format and print, x0 contains the address
+    ldr x0,qAdrszCarriageReturn //print \n 
+    bl affichageMess		// format and print, x0 contains the address
 
 
 
@@ -73,49 +73,51 @@ _start:                      // entry of program
 
 
 
-	//create file and open
-    mov x0,AT_FDCWD
-    ldr x1,qAdrsBuffer  // file name
-    mov x2,O_CREAT|O_RDWR        //  flag
-    ldr x3,qFicMask1             // Mode
-    mov x8,56                  // call system_open = 56 file
-    svc 0
-    mov x19,x0                   //save file descriptor
+    //create file and open
+    mov x0,AT_FDCWD		// flag to file
+    ldr x1,qAdrsBuffer  	// load file name
+    mov x2,O_CREAT|O_RDWR       // flag
+    ldr x3,qFicMask1            // Mode
+    mov x8,56                   // request open file, sys_open = 56 
+    svc 0			// call system
+    mov x19,x0                  // save file descriptor
 
     //write to file
-    ldr x1,qAdrsBuffer1
-    mov x2,x20                   // length to write 
-    mov x8, #64               // select system call 'write'
-    svc #0                       // perform the system call
+    ldr x1,qAdrsBuffer1		// load message to write
+    mov x2,x20                  // length to write 
+    mov x8, #64               	// request system write to file, sys_write = 64
+    svc #0                      // perform the system call
 
     //Close file
-    mov x0,x19                   // Fd  fichier 
-    mov x8, #57               //  call system CLOSE
-    svc #0
+    mov x0,x19                  // Fd   
+    mov x8, #57               	// request system to close file,  sys_close = 57
+    svc #0			// perform the system call
  
-100:   /* standard end of the program */
-    mov x0, #0             // return code
-    mov x8, #93          // request to exit program
-    svc 0                  // perform the system call
+100:   /* END */
+    mov x0, #0             	// return code
+    mov x8, #93          	// request to exit program
+    svc 0                  	// perform the system call
 
+    //format and print, x0 contains the address
 affichageMess:
     stp x0,x1,[sp,-16]!        // save  registers
     stp x2,x8,[sp,-16]!        // save  registers
     mov x2,0                   // size counter 
 1:                             // loop start
     ldrb w1,[x0,x2]            // load a byte
-    cbz w1,2f                  // if zero -> end string
-    add x2,x2,#1               // else increment counter
+    cbz w1,2f                  // if 0: end string
+    add x2,x2,#1               // else: counter++
     b 1b                       // and loop
 2:                             // x2 =  string size
 
     mov x1,x0                  // string address
-    mov x0,1              // output Linux standard STDOUT=1
-    mov x8,64               // code call system "write"=64
-    svc 0                      // call systeme Linux
+    mov x0,1                   // output Linux standard STDOUT=1
+    mov x8,64                  // request system write to file, sys_write = 64
+    svc 0                      // perform the system call
+    
     ldp x2,x8,[sp],16          // restaur  2 registres
     ldp x0,x1,[sp],16          // restaur  2 registres
-    ret                        // retour adresse lr x30
+    ret                        // return
 
 
 .align 4                   // instruction to realign the following routines
